8e75943f53f8628814020e7fc81678c2d2769bd6
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-08-08 23:11:09 +0700

    changed dependency params to pointer type

--- core/repository/UserRepository.go
+++ core/repository/UserRepository.go
@@ -11,26 +11,26 @@ type UserRepository struct {
 }
 
 func NewUserRepository(db *gorm.DB) domain.IUserRepository {
-	return UserRepository{Db: db}
+	return &UserRepository{Db: db}
 }
 
-func (u UserRepository) CheckUsernameExists(username string) bool {
+func (u *UserRepository) CheckUsernameExists(username string) bool {
 	result := u.Db.First(&domain.User{}, "username = ?", username)
 	return result.RowsAffected == 1
 }
 
-func (u UserRepository) GetById(id string) *domain.User {
+func (u *UserRepository) GetById(id string) *domain.User {
 	var user domain.User
 	u.Db.First(&user, "id = ?", id)
 	return &user
 }
 
-func (u UserRepository) Insert(user *domain.User) (string, error) {
+func (u *UserRepository) Insert(user *domain.User) (string, error) {
 	result := u.Db.Create(&user)
 	return user.ID, result.Error
 }
 
-func (u UserRepository) Delete(id string) error {
+func (u *UserRepository) Delete(id string) error {
 	result := u.Db.Delete(&domain.User{}, id)
 	return result.Error
 }
